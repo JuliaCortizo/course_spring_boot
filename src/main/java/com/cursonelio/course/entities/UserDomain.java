a126da70c5374575ccd5c2c82beb0364ec4566b9
jacg <jacg@gft.com>
2022-07-07 15:23:32 -0300

    H2 database, test profile, JPA

--- src/main/java/com/cursonelio/course/entities/UserDomain.java
+++ src/main/java/com/cursonelio/course/entities/UserDomain.java
@@ -3,21 +3,30 @@ package com.cursonelio.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserDomain implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
+	public UserDomain() {
 		
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public UserDomain(long id, String name, String email, String phone, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -78,7 +87,7 @@ public class User implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		UserDomain other = (UserDomain) obj;
 		return id == other.id;
 	}
 	
--- src/main/java/com/cursonelio/course/resources/UserResource.java
+++ src/main/java/com/cursonelio/course/resources/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cursonelio.course.entities.User;
+import com.cursonelio.course.entities.UserDomain;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "Ju", "d", "828282", "kakaka");
+	public ResponseEntity<UserDomain> findAll(){
+		UserDomain u = new UserDomain(1, "Ju", "d", "828282", "kakaka");
 		return ResponseEntity.ok().body(u);
 	}
 }
