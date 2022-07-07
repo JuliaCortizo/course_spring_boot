49638742d5bc144874bd820309ad2c49462baf6f
jacg <jacg@gft.com>
2022-07-07 14:12:35 -0300

    User entity and resource

---
+++ src/main/java/com/cursonelio/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.cursonelio.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursonelio.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "Ju", "d", "828282", "kakaka");
+		return ResponseEntity.ok().body(u);
+	}
+}
